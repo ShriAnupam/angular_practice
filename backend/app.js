740023bba56424fceea41e19cb6e568c6537de53
Anupam Shrivas <anupamshrivas009@gmail.com>
2022-05-02 23:36:01 +0530

    Created new request for backend send posts data from backend http://localhost:3000/api/posts path

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next)=>{
-  res.send('Hello From Express!')
+app.use('/api/posts',(req, res, next)=>{
+  // res.send('Hello From Express!')
+  const posts = [
+    {
+      id: 'aseff232234',
+      title:'First Server SIde Post',
+      content:'This is coming from the Server'
+    },
+    {
+      id: 'gdgd25234',
+      title:'Second Server SIde Post',
+      content:'This is coming from the Server'
+    }
+  ];
+  // res.json(posts);
+  res.status(200).json({
+    message:'Posts Fetched Successfully',
+    posts:posts
+  });
 })
 
 module.exports = app;
