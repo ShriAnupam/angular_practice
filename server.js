8c0a6b635ea08abc8a2079f7c18b540d9767b989
Anupam Shrivas <anupamshrivas009@gmail.com>
2022-05-02 22:50:08 +0530

    Adding Backend by Node Using Backend server as node express module And Server file as starting the server and calling express module by exports

---
+++ server.js
@@ -0,0 +1,11 @@
+const http  = require('http');
+const app = require('./backend/app');
+// const server = http.createServer((req, res)=>{
+//   res.end('This my First response');
+// })
+        // Global Variable
+const port = process.env.PORT || 3000;
+app.set('port',port);
+const server = http.createServer(app)
+
+server.listen(port);
